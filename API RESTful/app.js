539425a17deee3ef7fe68e2ab5eec12d21e1f0b9
Pablo Benedit <beneditpablo@gmail.com>
2022-07-20 18:46:33 -0300

    Add files via upload

---
+++ API RESTful/app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = require('./src/routes/routes')
+
+
+const app = express();
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api", router);
+app.use(express.static(__dirname +"/public"))
+
+const server = app.listen(8080, () => console.log(`Server UP on port 8080`))
+server.on('error', (err) => {
+console.log(`El servidor tuvo un error: ${error}`)})
